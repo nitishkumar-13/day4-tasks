bb42001efc51e38a35050e925bebaf21a7fbbe28
nitishkumar-13 <nitishkumars1302.com>
2024-02-24 22:33:51 +0530

    task 4

--- anonymous function & IIFE/on.js
+++
@@ -1,3 +0,0 @@
-const arr = [1,2,3,4,5,];
-const oddNumber = arr.filter((num) => num%2 === 1);
-console.log(oddNumber);
--- anonymous function & IIFE/rd.js
+++
@@ -1,5 +0,0 @@
-let chars = ["A", "B", "A", "C", "B"];
-let dupChars = chars.filter((c, index) => {
-  return chars.indexOf(c) !== index;
-});
-console.log(dupChars);
